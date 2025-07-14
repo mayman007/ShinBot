2d8763cd9eb84660186056e32bbd2f63b2628d29
mayman007 <medoems2210@gmail.com>
2025-07-15 02:56:52 +0300

    added gemini model var

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ with open("settings.yaml", "r") as file:
     BOT_USERNAME = config.get("BOT_USERNAME")
     ADMIN_IDS = config.get("ADMIN_IDS")
     GEMINI_API_KEY = config.get("GEMINI_API_KEY")
+    GEMINI_MODEL = config.get("GEMINI_MODEL")
     HUGGINGFACE_TOKEN = config.get("HUGGINGFACE_TOKEN")
     REDDIT_CLIENT_ID = config.get("REDDIT_CLIENT_ID")
     REDDIT_CLIENT_SECRET = config.get("REDDIT_CLIENT_SECRET")
