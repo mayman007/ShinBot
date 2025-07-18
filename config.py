b8d5b67e4d3b5de17e408ebc06028ab61e195031
mayman007 <medoems2210@gmail.com>
2025-07-18 12:49:12 +0300

    added BOT_NAME var

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ with open("settings.yaml", "r") as file:
     API_ID = config.get("API_ID")
     API_HASH = config.get("API_HASH")
     BOT_USERNAME = config.get("BOT_USERNAME")
+    BOT_NAME = config.get("BOT_NAME")
     ADMIN_IDS = config.get("ADMIN_IDS")
     FEEDBACK_CHAT_ID = config.get("FEEDBACK_CHAT_ID")
     GEMINI_API_KEY = config.get("GEMINI_API_KEY")
